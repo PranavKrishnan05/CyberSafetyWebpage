a8dc6c3448548b90cf67376cfc757e604b00e528
PranavKrishnan05 <Pranav.Krishnan05@gmail.com>
2023-06-21 00:43:29 -0500

    Added new file structure

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Card } from "../../components/blog/Card"
+import { Facts } from "../../components/facts/Facts"
+export const Home = () => {
+  return (
+    <>
+      {/*  <Slider />*/}
+      <Facts />
+      <Card />
+    </>
+  )
+}
