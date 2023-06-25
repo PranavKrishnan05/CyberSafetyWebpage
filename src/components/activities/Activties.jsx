65d8f0a4958529ff3ecb359f1d806b2247640331
SrisaivikasMunagala <45889356+SrisaivikasMunagala@users.noreply.github.com>
2023-06-25 16:01:16 -0500

    added password decipher activity

--- src/components/activities/Activties.jsx
+++ src/components/activities/Activties.jsx
@@ -20,14 +20,14 @@ export const Activities = () => {
         
       </div>
       </a>
-      <a href="">
+      <a href="https://passworddecipher.srisaivikasmuna.repl.co/">
 
       <div className="profile-card">
-        <img src="https://media.istockphoto.com/id/1273109788/vector/coming-soon-isolated-vector-icon-paper-style-promotion-sign-start-a-new-business-design.jpg?s=612x612&w=0&k=20&c=0APH6QCc371SuCEYLspgp6oh-tE5-rvbK0dzLMRmJGA=" alt="Srisaivikas" className="profile-image" />
+        <img src="https://th.bing.com/th/id/OIP.1J71HxbWkP52NB1a_Q5ERAHaHa?pid=ImgDet&w=1200&h=1200&rs=1" className="profile-image" />
         <div className="profile-details">
-          <h2>Coming Soon</h2>
+          <h2>Password Strength Activity</h2>
           <p>
-            Work in Progress...
+          The Password Strength Checker is an educational activity that helps users evaluate the strength of their passwords. By inputting their password, users receive an estimated crack time and a classification of their password strength. The sleek and modern design, inspired by FAANG companies, enhances the user experience, making it visually appealing and easy to navigate.
           </p>
         </div>
       </div>
